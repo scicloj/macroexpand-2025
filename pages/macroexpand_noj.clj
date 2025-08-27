a3ae08aa101b032c50386ef76c7f096f328ade97
daslu <daniel.slutsky@gmail.com>
2025-08-27 22:55:21 +0300

    improved macroexpand_noj page

--- pages/macroexpand_noj.clj
+++ pages/macroexpand_noj.clj
@@ -3,17 +3,42 @@
 
 ;; ## Macroexpand-Noj - growing the Noj ecosystem
 ;; 
-;; The Macroexpand-Noj conference is focused on the growth of the [Noj](https://scicloj.github.io/noj) toolkit for data science. It is one of the [Macroexpand 2025](./) pair of conferences.
+;; **📅 October 17-18, 2025 | Online Conference**
+;;
+;; The Macroexpand-Noj conference is focused on the growth of the [Noj](https://scicloj.github.io/noj) toolkit for data science. Noj is a Clojure toolkit that brings together data processing, visualization, and scientific computing libraries into a cohesive whole. This conference is one of the [Macroexpand 2025](./) pair of conferences organized by [Scicloj](https://scicloj.github.io/).
 ;; 
+;; ### About the Conference
+;;
+;; This two-day online event brings together practitioners, library authors, and data enthusiasts to share knowledge, showcase projects, and collaborate on growing Clojure's data science capabilities. Whether you're analyzing real-world datasets, building new tools, or documenting existing libraries, this conference is for you.
+;;
+;; ### Call for Proposals
 ;; 
-;; ### Preferred topics
-;; (tentative)
-;; 
-;; (Please see the [Speakers](./speakers.html) page for guidelines.)
+;; **Submission Deadline: September 8, 2025**
+;;
+;; We invite you to share your work with the Noj ecosystem and broader data science community. All talks must include reproducible code and clear documentation.
+;;
+;; [Submit Your Talk Proposal](https://forms.gle/rQj5cR1FPAYR7mA97){class="btn btn-gradient" target="_blank"}
+;;
+;; Please review our [speaker guidelines](./speakers.html) for detailed requirements and recommendations.
+;;
+;; ### Preferred Topics
 ;; 
 ;; The following will be prioritized in talk proposals:
 ;; 
-;; * Documentation of relevant libraries, theory, and methods
-;; * Real-world data analysis examples (including follow-ups from [SciNoj Light](https://scicloj.github.io/scinoj-light-1/))
-;; * New tools, libraries, and features
-;; * Broader topics of data processing, data engineering, visual tooling, data visualization, data modelling, scientific computing, and machine learning
+;; * **Documentation** - Making libraries, theory, and methods accessible to the community
+;; * **Real-world Analysis** - Practical data science examples and case studies (including follow-ups from [SciNoj Light](https://scicloj.github.io/scinoj-light-1/))
+;; * **Tools & Libraries** - New features, integrations, and ecosystem improvements
+;; * **Data Engineering** - Processing pipelines, ETL, and data infrastructure
+;; * **Visualization** - Interactive graphics, dashboards, and visual analytics
+;; * **Scientific Computing** - Numerical methods, statistics, and domain-specific applications
+;; * **Machine Learning** - Models, workflows, and ML engineering with Clojure
+;;
+;; ### Target Audience
+;;
+;; General Clojure programmers interested in data science, from beginners exploring data tools to experienced practitioners sharing advanced techniques. Talks should be accessible while providing depth and practical value.
+;;
+;; ### Connect & Discuss
+;;
+;; Join the conversation at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/) where we coordinate projects and help each other with data science in Clojure.
+;;
+;; [Questions? Let's talk](https://scicloj.github.io/docs/community/contact/){class="btn btn-gradient" target="_blank"}
